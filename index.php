ea2b7694c296034ec009f888c2cbfbfba30a6642
David Heredia <davidheredia933@hotmail.com>
2017-05-10 03:37:17 +0000

    update1

--- index.php
+++ index.php
@@ -6,29 +6,29 @@
   <link rel="stylesheet" href="style.css" />
  </head>
  <body>
-  <h1 style="text-align:center;">Servicios</h1>
+  <h1 style="text-align:center;">Productos</h1>
   <table>
    <?php
   $servername = "us-cdbr-iron-east-03.cleardb.net";
-  $username = "b74ba3320e82ec";
-  $password = "2d194843";
-  $dbname = "ad_bb6ddb2f34daf52";
+  $username = "bfb29bf957d2bf";
+  $password = "5f7915c8";
+  $dbname = "ad_8e9e7dd597e550c";
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
   }
-  $sql = "select * from servicios";
+  $sql = "SELECT * from Productos";
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {?>
       <tr>
-      <td><?php echo $row['codigo_servicio']?></td>
-      <td><?php echo $row['nombre_servicio']?></td>
-      <td><?php echo $row['descripcion_servicio']?></td>
-      <td><?php echo $row['imagen_servicio']?></td>
+      <td><?php echo $row['idProductos']?></td>
+      <td><?php echo $row['nombre']?></td>
+      <td><?php echo $row['categoria']?></td>
+       <td><?php echo $row['precio']?></td>
       </tr>
      <?php }
   } else {
